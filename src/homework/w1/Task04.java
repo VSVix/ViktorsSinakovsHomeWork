0352c101674a1419b191d44d7426d586506d43f9
Viktors Sinakovs <viktors.sinakovs@ingenico.com>
2018-04-25 23:18:00 +0300

    Task 4

--- src/homework/w1/Task04.java
+++ src/homework/w1/Task04.java
@@ -1,4 +1,8 @@
 package homework.w1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 /*
 Virknes konvertēšana
 
@@ -17,4 +21,27 @@ Virknei ir jābūt skaitļiem atdalītiem ar atstarpi (piemēram, "11 2 36.4 4 8
 Virkni ir nepieciešams sadalīt un konvertēt skaitļos, kuru summu pēc tam izvada uz ekrāna.
  */
 public class Task04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input numbers splitted with space:");
+        String input = scanner.nextLine();
+
+        try {
+            Double stringSum = sumDouble(input);
+            System.out.println("All numbers SUM = " + stringSum);
+        } catch (NumberFormatException ex) {
+            System.out.println("Inputted not a number - " + ex.getMessage().substring(18));
+        }
+
+    }
+
+    private static Double sumDouble(String input) {
+        Double sum = 0.;
+        String[] elements = input.split("\\s+");
+
+        for (int i = 0; i < elements.length; i++) {
+            sum += Double.parseDouble(elements[i]);
+        }
+        return sum;
+    }
 }
